d49c819778e0712cab7888b0c3814306bde394fb
gorpig <gorpig@localhost.localdomain>
2020-07-12 14:13:44 -0600

    [rfc3579][EAP-Message-Attr] Adding RFC3579 Attribute support

---
+++ rfc3579/generate.go
@@ -0,0 +1,8 @@
+//go run ../cmd/radius-dict-gen/main.go -package rfc3579 -output generated.go  /path/to/dictionary.rfc3579
+//
+// grokked from https://github.com/FreeRADIUS/freeradius-client/blob/master/etc/dictionary
+//
+//ATTRIBUTE	EAP-Message		79	string
+//ATTRIBUTE	EAP-Key-Name		102	string
+
+package rfc3579
